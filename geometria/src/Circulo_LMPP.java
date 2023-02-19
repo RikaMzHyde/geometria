d545d5b283109003b33c4d0c17fd6f631f43f2ae
Laura <laurampp95@gmail.com>
2023-02-19 23:04:10 +0100

    versión refactorizada

--- geometria/src/Circulo_LMPP.java
+++ geometria/src/Circulo_LMPP.java
@@ -1,13 +1,12 @@
 
-public class Circulo extends FiguraGeometrica {
+public class Circulo_LMPP extends FiguraGeometrica_LMPP {
 	private double radio;
 	//Constante pi
 	static final double PI = 3.1416;
 	
-	public Circulo(double r, String tipoFigura) {
+	public Circulo_LMPP(double r, String tipoFigura) {
 		super(tipoFigura);
 		radio = r;
-		
 	}
 
 	@Override
